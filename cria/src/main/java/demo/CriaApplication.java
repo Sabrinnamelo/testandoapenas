0968b2ef64bd31ed30974ddbd15fe23488433a31
Sabrinnamelo <smsm00380@gmail.com>
2024-03-02 12:32:09 -0300

    olá escola

---
+++ cria/src/main/java/demo/CriaApplication.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriaApplication.class, args);
+	}
+
+}
---
+++ cria/src/main/java/model/Escola.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Escola {
+
+	public static void main(String[] args) {
+		String escola ="cria";
+	}
+
+}
